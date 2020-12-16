b96af249ac02bac98c11048cdca425c86ebce570
Udaya N <udayaprakash2899@gmail.com>
2020-12-16 13:52:41 +0530

    Renamed field usage in connection

--- connection/connection.go
+++ connection/connection.go
@@ -13,7 +13,7 @@ import (
 func NewConnection(dbConfig configuration.DatabaseConfiguration) (*sql.DB, error) {
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
-		dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName)
+		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.Name)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
